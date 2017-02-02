f6efe8e3050e26760e5e67f9cd5aef1b6440d20b
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-02 18:28:58 +0700

    Add routes to contact and about page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('pages.index');
 });
 
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
 Route::get('/login', function () {
     return view('pages.login');
 });
