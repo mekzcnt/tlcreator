6bf0a2ea9cfe8e52f6931eb6e466b6486f71c775
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-03 04:49:19 +0700

    move @section('list_nav') to home.blade.php

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,26 +2,6 @@
 
 @section('title', 'Home')
 
-@section('list_nav')
-  @if (Auth::guest())
-      <li class="active"><a href="{{ url('/') }}">Home</a></li>
-      <li><a href="{{ url('/about') }}">About</a></li>
-      <li><a href="{{ url('/contact') }}">Contact</a></li>
-      <li><a href="{{ url('/login') }}">Login</a></li>
-      <li><a href="{{ url('/register') }}">Register</a></li>
-  @else
-      <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-              {{ Auth::user()->name }} <span class="caret"></span>
-          </a>
-
-          <ul class="dropdown-menu" role="menu">
-              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-          </ul>
-      </li>
-  @endif
-@endsection
-
 @section('before_container')
   <!-- Carousel -->
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
