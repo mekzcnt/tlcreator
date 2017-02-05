63748535d45b28be874c2897340c1fcd7a1baa96
mekzcnt <mekzconnecting@gmail.com>
2017-02-05 17:52:09 +0000

    create AdminUsersController.php and add .gitignore to some path

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class AdminUserController extends Controller
+class AdminUsersController extends Controller
 {
     /**
      * Display a listing of the resource.
