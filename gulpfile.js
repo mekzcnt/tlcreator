2288ca0d9d80f779d7d9fc26c9b01a1ce6f192c9
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-06 07:50:39 +0700

    edit gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,19 +15,19 @@ elixir(function(mix) {
     mix.sass('app.scss')
 
         .styles([
-                'libs/blog-post.css',
-                'libs/bootstrap.css',
-                'libs/font-awesome.css',
-                'libs/metisMenu.css',
-                'libs/sb-admin-2.css'
+                './libs/blog-post.css',
+                './libs/bootstrap.css',
+                './libs/font-awesome.css',
+                './libs/metisMenu.css',
+                './libs/sb-admin-2.css'
             ], './public/css/libs.css')
 
         .scripts([
-                'libs/jquery.js',
-                'libs/bootstrap.js',
-                'libs/metisMenu.js',
-                'libs/sb-admin-2.js',
-                'libs/scripts.js'
+                './libs/jquery.js',
+                './libs/bootstrap.js',
+                './libs/metisMenu.js',
+                './libs/sb-admin-2.js',
+                './libs/scripts.js'
             ], './public/js/libs.js')
 
 });
