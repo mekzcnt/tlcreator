bd824517c82a83bc9e2247ae82977839d788dcdf
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-06 08:41:02 +0700

    test route to views/admin/index

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,6 +27,10 @@ Route::get('/login', function () {
      return view('pages.login');
 });
 
+Route::get('admin', function(){
+    return view('admin.index')
+});
+
 
 Route::auth();
 Route::get('/feed', 'HomeController@index');
