62a5e1fc33a0f3a9626b2f751a3607deb6fa24b5
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-12 04:44:39 +0700

    testing laravelcollective form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.admin')
 
 
-
-
 @section('content')
 
 
@@ -13,6 +11,22 @@
          <div class="form-group">
                 {!! Form::label('name', 'Name:') !!}
                 {!! Form::text('name', null, ['class'=>'form-control'])!!}
+         </div>
+
+
+         <div class="form-group">
+           {!! Form::label('email', 'Email:') !!}
+           {!! Form::email('email', null, ['class'=>'form-control'])!!}
+         </div>
+
+         <div class="form-group">
+           {!! Form::label('role_id', 'Role:') !!}
+           {!! Form::select('role_id', [''=>'Choose Options'] + $roles , null, ['class'=>'form-control'])!!}
+         </div>
+
+         <div class="form-group">
+           {!! Form::label('is_active', 'Status:') !!}
+           {!! Form::select('is_active', array(1 => 'Active', 0=> 'Not Active'), 0 , ['class'=>'form-control'])!!}
          </div>
 
          <div class="form-group">
