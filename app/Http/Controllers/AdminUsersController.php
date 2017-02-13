22e1e19afada269abd1f7320f33e8d4a9e20aa34
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-13 18:07:53 +0700

    fix create user

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -60,10 +60,12 @@ class AdminUsersController extends Controller
         // }
         //
         // User::create($input);
-        // return redirect('/admin/users');
 
 
-        return $request->all();
+
+        User::create($request->all());
+
+        return redirect('/admin/users');
 
     }
 
