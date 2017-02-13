24ec48abfdba0492f0073459af52c3d76bfe9241
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-13 19:05:04 +0700

    test

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -45,25 +45,25 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
+        $input = $request->all();
         // if(trim($request->password) == ''){
         //     $input = $request->except('password');
         // } else{
-        //     $input = $request->all();
-        //     $input['password'] = bcrypt($request->password);
-        // }
-        //
-        // if($file = $request->file('photo_id')) {
-        //     $name = time() . $file->getClientOriginalName();
-        //     $file->move('images', $name);
-        //     $photo = Photo::create(['file'=>$name]);
-        //     $input['photo_id'] = $photo->id;
-        // }
-        //
-        // User::create($input);
 
-        if($request->file('photo_id')) {
-          return "photo exist";
+        // }
+        //
+        if($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
         }
+
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
+
+
 
         // User::create($request->all());
         //
