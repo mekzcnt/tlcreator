c2f6bace983198dd0627f4cee8cac6c333ca6d03
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-14 20:11:38 +0700

    edit route.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,12 +30,12 @@ Route::get('/contact', function () {
 Route::auth();
 Route::get('/feed', 'HomeController@index');
 
-Route::get('/admin', function(){
-     return view('admin.index');
-});
-
 Route::group(['middleware'=>'admin'], function() {
 
-      Route::resource('admin/users', 'AdminUsersController');
+    Route::get('/admin', function(){
+        return view('admin.index');
+    });
+
+    Route::resource('admin/users', 'AdminUsersController');
 
 });
