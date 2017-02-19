805e0d7f3f26b60a8511f76db0e4a2709816be01
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-19 23:25:07 +0700

    edit User.php

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
     }
 
     public function isAdmin() {
-      if($this->role->name == "Administrator" && $this->is_active == 1){
+      if($this->role->name == 'Administrator' && $this->is_active == 1){
         return true;
       }
       return false;
