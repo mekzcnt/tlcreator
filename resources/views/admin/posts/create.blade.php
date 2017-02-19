d8ead78aa3aa8e94549139783e1f6d41b9fbc420
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-20 00:58:41 +0700

    edit admin posts create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -20,19 +20,19 @@
 
         <div class="form-group">
             {!! Form::label('category_id', 'Category:') !!}
-            {!! Form::select('category_id', [''=>'Choose Categories'] + $categories, null, ['class'=>'form-control'])!!}
+            {!! Form::select('category_id', array(''=>'Options'), null, ['class'=>'form-control'])!!}
         </div>
 
 
         <div class="form-group">
             {!! Form::label('photo_id', 'Photo:') !!}
-            {!! Form::file('photo_id', null,['class'=>'form-control'])!!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control'])!!}
          </div>
 
 
         <div class="form-group">
-            {!! Form::label('body', 'Description:') !!}
-            {!! Form::textarea('body', null, ['class'=>'form-control'])!!}
+            {!! Form::label('description', 'Description:') !!}
+            {!! Form::textarea('description', null, ['class'=>'form-control'])!!}
         </div>
 
 
