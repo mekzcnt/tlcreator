299dacb984df41127b402779284640f7b5f99f05
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-19 23:43:30 +0700

    edit admin post index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -30,13 +30,13 @@
 
           <tr>
               <td>{{$post->id}}</td>
-              <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400' }} " alt=""></td>
+              <td><img height="50" src="#" alt=""></td>
               <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->user->name}}</a></td>
               <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
               <td>{{$post->title}}</td>
               <td>{{str_limit($post->body, 30)}}</td>
-              <td><a href="{{route('home.post', $post->slug)}}">View Post</a></td>
-              <td><a href="{{route('admin.comments.show', $post->id)}}">View Comments</a></td>
+              <td><a href="#">View Post</a></td>
+              <td><a href="#">View Comments</a></td>
               <td>{{$post->created_at->diffForhumans()}}</td>
               <td>{{$post->updated_at->diffForhumans()}}</td>
 
