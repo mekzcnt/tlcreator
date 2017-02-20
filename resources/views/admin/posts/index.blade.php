24504cf418718d646efb0c530ce50d068c825b01
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-20 14:36:23 +0700

    show images in admin post index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,12 +10,12 @@
     <table class="table">
        <thead>
          <tr>
-             <th>Id</th>
+             <th>ID</th>
+             <th>Photo</th>
              <th>User</th>
              <th>Category</th>
-             <th>Photo</th>
              <th>Title</th>
-             <th>Description</th>
+             <!-- <th>Description</th> -->
              <th>Created at</th>
              <th>Update</th>
         </thead>
@@ -28,11 +28,11 @@
 
           <tr>
               <td>{{$post->id}}</td>
+              <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400' }} " alt=""></td>
               <td>{{$post->user->name}}</td>
               <td>{{$post->category_id}}</td>
-              <td>{{$post->photo_id}}</td>
               <td>{{$post->title}}</td>
-              <td>{{str_limit($post->description, 30)}}</td>
+              <!-- <td>{{str_limit($post->description, 30)}}</td> -->
               <td>{{$post->created_at->diffForhumans()}}</td>
               <td>{{$post->updated_at->diffForhumans()}}</td>
 
