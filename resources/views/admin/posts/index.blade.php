ca1962194bcc214a25f997ad091a59c88fc30861
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-20 15:04:48 +0700

    able to display category from db

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -30,7 +30,7 @@
               <td>{{$post->id}}</td>
               <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400' }} " alt=""></td>
               <td>{{$post->user->name}}</td>
-              <td>{{$post->category_id}}</td>
+              <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
               <td>{{$post->title}}</td>
               <!-- <td>{{str_limit($post->description, 30)}}</td> -->
               <td>{{$post->created_at->diffForhumans()}}</td>
