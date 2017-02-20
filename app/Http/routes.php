8a317bd839c2b09a0a611cc19c99fc62567ba91f
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-21 01:28:03 +0700

    add routes for admin media page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,5 +39,6 @@ Route::group(['middleware'=>'admin'], function() {
     Route::resource('admin/users', 'AdminUsersController');
     Route::resource('admin/posts', 'AdminPostsController');
     Route::resource('admin/categories', 'AdminCategoriesController');
+    Route::resource('admin/media', 'AdminMediasController');
 
 });
