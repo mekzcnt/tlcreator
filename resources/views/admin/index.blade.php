a313044dc24f2de154fdcee335ee9e1318a6bf45
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-21 02:20:55 +0700

    edit admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-    <h1>Admin</h1>
+    <h1>Welcome back to Admin System!</h1>
+
+    <p>Now you able to use these features</p>
+    <ul>
+        <li>Add/Edit/Delete users</li>
+        <li>Add/Edit/Delete (timeline) posts</li>
+        <li>Add/Edit/Delete categories</li>
+        <li>Add/Edit/Delete medias (photo only)</li>
+    </ul>
 
 @stop
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,6 +16,8 @@
 
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -23,6 +25,7 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
 
     @yield('header')
 
@@ -42,7 +45,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">Timeline Creator (Admin Panel)</a>
+            <a class="navbar-brand" href="/">Timeline Creator</a>
         </div>
         <!-- /.navbar-header -->
 
