484b60d333f9dcc0591c1bbf821972dcbec6734a
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-02-21 09:41:56 +0700

    edit

--- resources/views/timeline-post.blade.php
+++ resources/views/timeline-post.blade.php
@@ -14,6 +14,7 @@
         <span class="glyphicon glyphicon-time"></span> {{$post->created_at->diffForHumans()}}</span>
     </p>
     <hr>
+    <img src="https://f.ptcdn.info/652/016/000/1394692365-updatetime-o.jpg">
     <p><strong>Description :</strong></p>
     <p>{{$post->description}}</p>
 </div>
