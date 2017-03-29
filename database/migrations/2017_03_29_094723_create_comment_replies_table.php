bd1fe5acebe10219a7bb0a2420e8bb3191a0ff77
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-03-29 16:58:44 +0700

    edit migration variables of comments and replies

--- database/migrations/2017_03_29_094723_create_comment_replies_table.php
+++ database/migrations/2017_03_29_094723_create_comment_replies_table.php
@@ -13,8 +13,15 @@ class CreateCommentRepliesTable extends Migration
     public function up()
     {
         Schema::create('comment_replies', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+          $table->increments('id');
+          $table->integer('comment_id')->unsigned()->index();
+          $table->integer('is_active')->default(0);
+          $table->string('author');
+          $table->string('email');
+          $table->text('body');
+          $table->timestamps();
+
+          $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
         });
     }
 
