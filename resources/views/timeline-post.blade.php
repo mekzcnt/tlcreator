e5cfa30f31910bfffda1fda7ac79b42c6a9efcb5
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-05 17:23:01 +0700

    edit comment photo user path

--- resources/views/timeline-post.blade.php
+++ resources/views/timeline-post.blade.php
@@ -30,47 +30,48 @@
 
 @section('comment')
 
-@if(Session::has('comment_message'))
-  {{session('comment_message')}}
-@endif
-<!-- Comments Form -->
-<div class="well">
-    <h4>Leave a Comment:</h4>
-
-    {!! Form::open(['method'=>'POST', 'action'=> 'PostCommentsController@store']) !!}
-
-        <input type="hidden" name="post_id" value="{{$post->id}}">
-
-         <div class="form-group">
-             {!! Form::label('body', 'Comments:') !!}
-             {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>3])!!}
-         </div>
-
-         <div class="form-group">
-             {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
-         </div>
-    {!! Form::close() !!}
-</div>
-
-<hr>
-
-<!-- Posted Comments -->
-
-@if(count($comments) > 0)
-
-        @foreach($comments as $comment)
-        <!-- Comment -->
-        <div class="media">
-            <a class="pull-left" href="#">
-                <img height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">{{$comment->author}}
-                    <small>{{$comment->created_at->diffForHumans()}}</small>
-                </h4>
-                <p>{{$comment->body}}</p>
-            </div>
-        </div>
-        @endforeach
+  @if(Session::has('comment_message'))
+    {{session('comment_message')}}
   @endif
+  <!-- Comments Form -->
+  <div class="well">
+      <h4>Leave a Comment:</h4>
+
+      {!! Form::open(['method'=>'POST', 'action'=> 'PostCommentsController@store']) !!}
+
+          <input type="hidden" name="post_id" value="{{$post->id}}">
+
+           <div class="form-group">
+               {!! Form::label('body', 'Comments:') !!}
+               {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>3])!!}
+           </div>
+
+           <div class="form-group">
+               {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
+           </div>
+      {!! Form::close() !!}
+  </div>
+
+  <hr>
+
+  <!-- Posted Comments -->
+
+  @if(count($comments) > 0)
+
+          @foreach($comments as $comment)
+          <!-- Comment -->
+          <div class="media">
+              <a class="pull-left" href="#">
+                  <img height="64" class="media-object" src="{{$comment->photo}}" alt="">
+              </a>
+              <div class="media-body">
+                  <h4 class="media-heading">{{$comment->author}}
+                      <small>{{$comment->created_at->diffForHumans()}}</small>
+                  </h4>
+                  <p>{{$comment->body}}</p>
+              </div>
+          </div>
+          @endforeach
+  @endif
+
 @endsection
