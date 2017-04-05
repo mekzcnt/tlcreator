992adbafcb349763618b7cf8878cbd08a3f91772
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-05 16:10:02 +0700

    example table on admin view comments

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -4,6 +4,26 @@
 @section('content')
 
 <h1>Comments</h1>
-
-
-@stop
+<table class="table">
+   <thead>
+     <tr>
+        <th>ID</th>
+        <th>Author</th>
+        <th>Email</th>
+        <th>Body</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+         <td>John</td>
+         <td>Doe</td>
+         <td>john@example.com</td>
+      </tr>
+      <tr>
+         <td>Mary</td>
+         <td>Moe</td>
+         <td>marry@example.com</td>
+      </tr>
+    </tbody>
+</table>
+@endsection
