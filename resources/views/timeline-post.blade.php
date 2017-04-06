9ed307559a9719ea0f4c5c5ee8617e706795849e
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-06 17:12:01 +0700

    testing reply comment on timeline post

--- resources/views/timeline-post.blade.php
+++ resources/views/timeline-post.blade.php
@@ -70,33 +70,41 @@
                       <small>{{$comment->created_at->diffForHumans()}}</small>
                   </h4>
                   <p>{{$comment->body}}</p>
-                  <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
 
-                        {!! Form::open(['method'=>'POST', 'action'=> 'CommentRepliesController@createReply']) !!}
 
-                             <div class="form-group">
-                                <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                                 {!! Form::label('body', 'Reply:') !!}
-                                 {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1])!!}
-                             </div>
+                  @if(count($comment->replies) > 0)
 
-                             <div class="form-group">
-                                 {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
-                             </div>
-                        {!! Form::close() !!}
+                      @foreach($comment->replies as $reply)
+                        <!-- Nested Comment -->
+                          <div class="media">
+                              <a class="pull-left" href="#">
+                                  <img class="media-object" src="http://placehold.it/64x64" alt="">
+                              </a>
+                              <div class="media-body">
+                                  <h4 class="media-heading">Nested Start Bootstrap
+                                      <small>August 25, 2014 at 9:30 PM</small>
+                                  </h4>
+                                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                              </div>
 
-                    </div>
-                  <!-- End Nested Comment -->
+                              {!! Form::open(['method'=>'POST', 'action'=> 'CommentRepliesController@createReply']) !!}
+
+                                   <div class="form-group">
+                                      <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                                       {!! Form::label('body', 'Reply:') !!}
+                                       {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1])!!}
+                                   </div>
+
+                                   <div class="form-group">
+                                       {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
+                                   </div>
+                              {!! Form::close() !!}
+
+                          </div>
+                        <!-- End Nested Comment -->
+                      @endforeach
+                  @endif
+
               </div>
           </div>
           @endforeach
