6ed8c79f012cc0e5d6c79bd1b3a36479fb71c0f1
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-12 13:53:29 +0700

    upgrade to laravel 5.3

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function(){
-  return view('admin.index');
+Route::group(['middleware'=>'admin'], function() {
+
+    Route::get('/admin', function(){
+      return view('admin.index');
+    });
+
+    Route::resource('admin/users', 'AdminUsersController');
+    Route::resource('admin/posts', 'AdminPostsController');
+    Route::resource('admin/categories', 'AdminCategoriesController');
+    Route::resource('admin/media', 'AdminMediasController');
+
+    Route::resource('admin/comments', 'PostCommentsController');
+    Route::resource('admin/comments/replies', 'CommentRepliesController');
+
 });
