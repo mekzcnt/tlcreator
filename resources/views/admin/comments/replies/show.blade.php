dcdfe229fd0c4fec3c7675c4e56323308ba095d7
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-12 12:36:03 +0700

    edit to CommentRepliesController

--- resources/views/admin/comments/replies/show.blade.php
+++ resources/views/admin/comments/replies/show.blade.php
@@ -42,7 +42,7 @@
               @else
 
 
-                  {!! Form::open(['method'=>'PATCH', 'action'=> ['CommentsRepliesController@update', $reply->id]]) !!}
+                  {!! Form::open(['method'=>'PATCH', 'action'=> ['CommentRepliesController@update', $reply->id]]) !!}
 
 
                   <input type="hidden" name="is_active" value="1">
@@ -59,7 +59,7 @@
               @endif
             </td>
             <td>
-              {!! Form::open(['method'=>'DELETE', 'action'=> ['CommentsRepliesController@destroy', $reply->id]]) !!}
+              {!! Form::open(['method'=>'DELETE', 'action'=> ['CommentRepliesController@destroy', $reply->id]]) !!}
 
               <div class="form-group">
                   {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
