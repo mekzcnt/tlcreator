d4fde9c56d881a002d038710df060229a24a61c6
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-12 21:14:42 +0700

    add paginate on admin view users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -54,5 +54,10 @@
        </tbody>
      </table>
 
+     <div class="row">
+         <div class="col-sm-6 col-sm-offset-5">
+             {{ $users->render() }}
+         </div>
+     </div>
 
 @stop
