fe035a0af573b18bf06fe2e35f761412d679e199
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-12 20:38:42 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -72,10 +72,10 @@ Route::group(['middleware'=>'admin', ], function() {
     ]]);
 
     Route::resource('admin/comment/replies', 'CommentRepliesController',['names'=>[
-        'index'=>'admin.replies.index',
-        'create'=>'admin.replies.create',
-        'store'=>'admin.replies.store',
-        'edit'=>'admin.replies.edit'
+        'index'=>'admin.comments.replies.index',
+        'create'=>'admin.comments.replies.create',
+        'store'=>'admin.comments.replies.store',
+        'edit'=>'admin.comments.replies.edit'
     ]]);
 
 });
