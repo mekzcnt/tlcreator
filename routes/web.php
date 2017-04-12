24f967d07381c4b031ece5201ff40be92a904855
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-12 20:33:08 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,53 @@ Route::get('/feed', 'HomeController@index');
 
 Route::get('/timeline/{id}', ['as'=>'feed.timeline', 'uses'=>'AdminPostsController@post']);
 
-Route::group(['middleware'=>'admin', 'as'=>'admin.'], function() {
+Route::group(['middleware'=>'admin', ], function() {
 
     Route::get('/admin', function(){ return view('admin.index');});
 
-    Route::resource('admin/users', 'AdminUsersController');
-    Route::resource('admin/posts', 'AdminPostsController');
-    Route::resource('admin/categories', 'AdminCategoriesController');
-    Route::resource('admin/media', 'AdminMediasController');
+    Route::resource('admin/users', 'AdminUsersController',['names'=>[
+        'index'=>'admin.users.index',
+        'create'=>'admin.users.create',
+        'store'=>'admin.users.store',
+        'edit'=>'admin.users.edit'
+    ]]);
 
-    Route::resource('admin/comments', 'PostCommentsController');
-    Route::resource('admin/comments/replies', 'CommentRepliesController');
+    Route::resource('admin/posts', 'AdminPostsController',['names'=>[
+        'index'=>'admin.posts.index',
+        'create'=>'admin.posts.create',
+        'store'=>'admin.posts.store',
+        'edit'=>'admin.posts.edit'
+    ]]);
+
+    Route::resource('admin/categories', 'AdminCategoriesController',['names'=>[
+        'index'=>'admin.categories.index',
+        'create'=>'admin.categories.create',
+        'store'=>'admin.categories.store',
+        'edit'=>'admin.categories.edit'
+    ]]);
+
+    Route::resource('admin/media', 'AdminMediasController',['names'=>[
+        'index'=>'admin.media.index',
+        'create'=>'admin.media.create',
+        'store'=>'admin.media.store',
+        'edit'=>'admin.media.edit'
+    ]]);
+
+
+    Route::resource('admin/comments', 'PostCommentsController',['names'=>[
+        'index'=>'admin.comments.index',
+        'create'=>'admin.comments.create',
+        'store'=>'admin.comments.store',
+        'edit'=>'admin.comments.edit',
+        'show'=>'admin.comments.show'
+    ]]);
+
+    Route::resource('admin/comment/replies', 'CommentRepliesController',['names'=>[
+        'index'=>'admin.replies.index',
+        'create'=>'admin.replies.create',
+        'store'=>'admin.replies.store',
+        'edit'=>'admin.replies.edit'
+    ]]);
 
 });
 
