b14f4658bff1092edf92560d466955f1aceb336a
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-18 02:10:40 +0700

    edit

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,5 @@ mix.scripts([
     'resources/assets/js/libs/bootstrap.js',
     'resources/assets/js/libs/metisMenu.js',
     'resources/assets/js/libs/sb-admin-2.js',
-    'resources/assets/js/libs/jquery.js'
 
 ], 'public/js/libs.js');
