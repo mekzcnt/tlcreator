4bfbf44be32d60ca7e848ee13470b20e65af2449
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-19 12:33:04 +0700

    test timelinejs

--- resources/views/timeline-post.blade.php
+++ resources/views/timeline-post.blade.php
@@ -139,8 +139,10 @@
   <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
 
   <script type="text/javascript">
-
-    var timeline_json = '{{$post->timeline}}'; // you write this part
+    function make_the_json = {
+      return JSON.parse('{{$post->timeline}}');
+    }
+    var timeline_json = make_the_json(); // you write this part
     // two arguments: the id of the Timeline container (no '#')
     // and the JSON object or an instance of TL.TimelineConfig created from
     // a suitable JSON object
