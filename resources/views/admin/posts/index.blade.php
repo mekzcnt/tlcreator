002e37f2ea4028488522bb62ce672ba307a0e7a9
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-19 20:17:37 +0700

    move column in admin post index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,9 +9,9 @@
          <tr>
              <th>ID</th>
              <th>Photo</th>
-             <th>User</th>
-             <th>Category</th>
              <th>Title</th>
+             <th>Category</th>
+             <th>User</th>
              <th>Post link</th>
              <th>Comments</th>
              <th>Created at</th>
@@ -27,9 +27,9 @@
           <tr>
               <td>{{$post->id}}</td>
               <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400' }} " alt=""></td>
-              <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->user->name}}</a></td>
+              <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->title}}</a></td>
               <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
-              <td>{{$post->title}}</td>
+              <td>{{$post->user->name}}</td>
               <td><a href="{{route('feed.timeline', $post->slug)}}">View Post</a></td>
               <td><a href="{{route('admin.comments.show', $post->id)}}">View Comments</a></td>
               <td>{{$post->created_at->diffForhumans()}}</td>
