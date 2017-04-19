1af25998ed892741e9acaf9599dd2c4dbde6954c
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-19 20:02:53 +0700

    paginate in admin media index

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -42,7 +42,7 @@
 
         <div class="row">
             <div class="col-sm-6 col-sm-offset-5">
-                {{ $posts->render() }}
+                {{ $photo->render() }}
             </div>
         </div>
 
