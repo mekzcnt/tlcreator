9d796d363ef59e14c25b956e114e54d443e5b326
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-19 08:21:58 +0700

    add attribute 'timeline'

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title', 'description', 'category_id', 'photo_id',
+        'title', 'timeline', 'description', 'category_id', 'photo_id',
     ];
 
     public function user() {
