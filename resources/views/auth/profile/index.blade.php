bc757bc28a05bd52df252c50553f43e089e56193
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-20 16:50:30 +0700

    add h1 and title

--- resources/views/auth/profile/index.blade.php
+++ resources/views/auth/profile/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title', 'Profile')
+@section('title', 'Your Profile')
 
 @section('before_container')
 @stop
@@ -8,6 +8,8 @@
 @section('content')
     <div class="row">
       <div class="col-lg-12">
+        <h1 id="title-page" class="text-center">Your Profile</h1>
+        <hr>
         <p>{{ $currentUser->name }}</p>
         <p>{{ $currentUser->email }}</p>
         <p>{{ $currentUser->photo->id }}</p>
