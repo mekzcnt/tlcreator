4d88f44a11d24cbbb66f533e3f73b428142dcc3d
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-20 16:29:35 +0700

    create profile page and UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,45 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+  public function getProfile()
+  {
+      $user = auth()->user();
+      $viewData = [
+          'currentUser' => $user
+      ];
+
+      return view('profiles.profile', $viewData);
+  }
+
+  public function getUpdateProfile()
+  {
+      $viewData = [
+          'currentUser' => auth()->user()
+      ];
+
+      return view('profiles.editProfile', $viewData);
+  }
+
+  public function UpdateProfile(Request $request)
+  {
+      $this->validate($request, [
+          'name' => 'required|alpha',
+          'email' => 'required|email',
+          'username' => 'required|max:255',
+      ]);
+
+      $user = auth()->user();
+//        $user->fill($request->all());
+      $user->name = $request->name;
+      $user->email = $request->email;
+      $user->username = $request->username;
+      $user->sex = $request->sex;
+      $user->age = $request->age;
+      $user->occupation = $request->occupation;
+//        don't have photo_id yet.
+
+      $user->save();
+
+      return redirect('/profile');
+  }
 }
