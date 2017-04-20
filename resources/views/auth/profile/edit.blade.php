25578bf0a7000260c96781a095f6835ffd882afe
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-20 16:57:14 +0700

    add edit your profile page

---
+++ resources/views/auth/profile/edit.blade.php
@@ -0,0 +1,16 @@
+extends('layouts.master')
+
+@section('title', 'Edit Your Profile')
+
+@section('before_container')
+@stop
+
+@section('content')
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 id="title-page" class="text-center">Edit Your Profile</h1>
+        <hr>
+        <p></p>
+      </div>
+    </div>
+@stop
