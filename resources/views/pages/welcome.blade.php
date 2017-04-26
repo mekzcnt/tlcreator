ce8877ae3b7f35014d036553ae6dbf056bee9216
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-26 21:47:00 +0700

    edit text and layout

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -85,26 +85,6 @@
 
 @section('content')
 <div class="container">
-  <!-- row of columns -->
-  <div class="row">
-    <div class="col-md-4">
-      <h2></h2>
-      <p></p>
-      <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-    </div>
-    <div class="col-md-4">
-      <h2></h2>
-      <p>น</p>
-      <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-   </div>
-    <div class="col-md-4">
-      <h2>แชร์!</h2>
-      <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-    </div>
-  </div>
-</div> <!-- /container -->
-
     <!-- Three columns of text below the carousel -->
     <div class="row">
       <div class="col-lg-4">
