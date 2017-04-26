451a6cfcbf797ddaad96e9f36e4b9655ceaae4e1
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-26 18:14:57 +0700

    change redirectTo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/feed';
+    protected $redirectTo = '/admin';
     /**
      * Create a new controller instance.
      *
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/feed';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
