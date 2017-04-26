8b4039cc92beb933561d3b64d56369c4c055f5b1
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-26 20:56:40 +0700

    edit text

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -14,7 +14,8 @@
       width="100%"
       height="300"
       frameborder="0" style="border:0"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDnwk5BdpQR3lN4gC_WlijxLkK4Q8KLJXg&q=Faculty+of+Information+Technology,+Lat+Krabang,+Bangkok" allowfullscreen>
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDnwk5BdpQR3lN4gC_WlijxLkK4Q8KLJXg&q=Faculty+of+Information+Technology,+Lat+Krabang,+Bangkok"
+      allowfullsscreen>
     </iframe>
     <h2 class="text-center">ที่อยู่</h2>
     <p class="text-center">คณะเทคโนโลยีสารสนเทศ<br>สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง<br>เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง<br>เขตลาดกระบัง กรุงเทพมหานคร 10520</p>
