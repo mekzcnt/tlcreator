fc70cd4d19190d2f6bbd07e031c9d1840206a49a
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-26 16:46:34 +0700

    add form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,9 +6,32 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
     <h1 id="title-page" class="text-center">Contact</h1>
     <hr>
     <p>Contact page</p>
+    <table class="table table-bordered">
+        <tr>
+            <td>Name</td>
+            <td>
+                <input type="text" class="form-control" name="name" value="" autofocus>
+            </td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>
+                <input type="text" class="form-control" name="age" value="">
+            </td>
+        </tr>
+        <tr>
+            <td>Content</td>
+            <td>
+                <input type="text" class="form-control" name="occupation" value="">
+            </td>
+        </tr>
+    </table>
+    <button type="submit" class="btn btn-primary pull-right">Submit</button>
+  </div>
 </div>
 @endsection
