57d7f9f79d1764d3be662634f0fbc762b77b3837
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-27 15:16:41 +0700

    add create timeline button

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -27,6 +27,7 @@
             <li><a href="{{ url('/login') }}">Login</a></li>
             <li><a href="{{ url('/register') }}">Register</a></li>
         @else
+            <li><a href="#"><button type="button" class="btn btn-warning">Create Timeline</button></a></li>
             <li><a href="{{ url('/feed') }}">Timeline Feed</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -34,8 +35,8 @@
                 </a>
 
                 <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">My Timeline</a></li>
-                    <li><a href="#">Settings</a></li>
+                    <li><a href="{{ url('/profile') }}">My Profile</a></li>
+                    <li><a href="{{ url('/profile/edit') }}">Edit Profile</a></li>
                     <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                 </ul>
             </li>
