45d2003374627ff346580d723e023642dcbccab7
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-28 20:51:17 +0700

    edit css for profile page

--- resources/views/auth/profile/index.blade.php
+++ resources/views/auth/profile/index.blade.php
@@ -8,12 +8,23 @@
 @section('content')
     <div class="row">
       <div class="col-lg-12">
+        <div class="card hovercard">
+            <div class="card-background">
+                <img class="card-bkimg" alt="" src="{{$currentUser->photo ? $currentUser->photo->file
+                  : 'http://placehold.it/400x400' }}">
+            </div>
+            <div class="useravatar">
+                <img alt="" src="{{$currentUser->photo ? $currentUser->photo->file
+                  : 'http://placehold.it/400x400' }}">
+            </div>
+            <div class="card-info"> <span class="card-title">{{ $currentUser->name }}</span>
+          </div>
+        </div>
         <h1 id="title-page" class="text-center">Your Profile</h1>
         <hr>
         <p>{{ $currentUser->name }}</p>
         <p>{{ $currentUser->email }}</p>
-        <p><img height="50" src="{{$currentUser->photo ? $currentUser->photo->file
-          : 'http://placehold.it/400x400' }} " alt=""></p>
+        <p><img height="50" src=" " alt=""></p>
       </div>
     </div>
 @stop
