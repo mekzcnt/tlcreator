7bc5dc69ff31a1c76c287d0540b36e984ece82fb
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-28 17:03:51 +0700

    edit add nullable for username

--- database/migrations/2017_04_25_212850_add_profiles_to_users_table.php
+++ database/migrations/2017_04_25_212850_add_profiles_to_users_table.php
@@ -14,7 +14,7 @@ class AddProfilesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->unique();
+            $table->string('username')->nullable()->unique();
         });
     }
 
