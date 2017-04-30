227023ece75508a4b763e2effbc43b7156212f35
MEKZ-LAPTOP\mekzc <mekzconnecting@gmail.com>
2017-04-30 15:05:37 +0700

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,12 @@ class UserController extends Controller
 {
   public function getProfile()
   {
+      $posts = Post::all();
       $viewData = [
           'currentUser' => auth()->user()
       ];
 
-      return view('auth.profile.index', $viewData);
+      return view('auth.profile.index', compact('posts','viewData'));
   }
 
   public function getUpdateProfile()
